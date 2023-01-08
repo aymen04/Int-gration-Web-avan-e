69b5b04e376078bdd5e3a8c50089e60d30dddc12
aymen04 <aymenadline5@gmail.com>
2023-01-08 14:14:09 +0100

    Premier push

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("organiser").addEventListener("click", function() {
+  document.getElementById("formulaire").style.display = "block";
+});
+
+document.getElementById("organiser1").addEventListener("click", function() {
+  document.getElementById("formulaire").style.display = "block";
+});
+
+
+
+  document.getElementById("annuler").addEventListener("click", function() {
+    document.getElementById("formulaire").style.display = "none";
+  });
+
+  document.getElementById("envoyer").addEventListener("click", function() {
+    document.getElementById("formulaire").style.display = "none";
+  });
+
+
+  
